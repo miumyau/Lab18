1b0c279c4a947ea4566562463f9e85c7cdbbff3a
ArtiOnMoon <67146003+ArtiOnMoon@users.noreply.github.com>
2024-03-13 10:24:01 +0300

    123

--- Lab18/Program.cs
+++ Lab18/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите строку");
+            string str = Console.ReadLine();
+            Console.WriteLine(Check(str));
+            Console.ReadKey();
+        }
+
+        static string Check(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> dict = new Dictionary<char, char>
+            {
+                {'(',')'},
+                {'[',']'},
+                {'{','}'}
+
+            };
+
+
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                { stack.Push(dict[c]); }
+
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    if (stack.Count==0||stack.Pop()!=c)
+                    {
+                        return "Скобки расставлены некорректно";
+                    }
+                    
+                }
+
+            }
+
+            if (stack.Count==0)
+            {
+                return "Скобки расставлены корректно";
+            }
+
+            else return "Скобки расставлены некорректно";
         }
     }
 }
